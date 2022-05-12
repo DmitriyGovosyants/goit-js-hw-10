7d99a335fa47c5b0f91f79a52a5d297f3c1d0b58
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-12 19:56:11 +0300

    add throw error when response is not ok

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,5 +3,11 @@ const option = '?fields=name,capital,population,flags,languages';
     
 export function fetchCountries(name) {
     return fetch(`${BASE_URL}name/${name}${option}`)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            
+            throw new Error(response.statusText);
+        })
 }
